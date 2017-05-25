086d62a809aac82d6dbcc659d93de292f9cd5bc2
Angela Coates <angela@angelamarii.com>
2017-05-24 21:38:20 -0400

    started trying to implement omniauth for instagram

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET'], scope: 'basic+media+public_content+follower_list+comments+relationships+likes'
+
 end
