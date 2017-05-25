ea5baa84bc2a5a5f5990aa17e18b4d2196c755f9
Angela Coates <angela@angelamarii.com>
2017-05-25 13:55:46 -0400

    added authenticate before action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate!, except: [:landing]
   def index
   end
 
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,7 +9,7 @@ class SessionController < ApplicationController
     self.current_user = User.from_omniauth(request.env['omniauth.auth'])
 
     if current_user
-      redirect_to root_path
+      redirect_to home_home_path
     else
       redirect_to auth_path(provider: 'facebook')
     end
